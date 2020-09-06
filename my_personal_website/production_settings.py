bde075a9a8955a63cf10b5b508a837b51b2adad6
Andrew Lin <andrewlin1722@gmail.com>
2020-09-05 20:40:15 -0500

    first commit

---
+++ my_personal_website/production_settings.py
@@ -0,0 +1,9 @@
+from my_personal_website.common import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+SECRET_KEY = os.environ['SECRET_KEY']
+
+#update this to be heroku
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
