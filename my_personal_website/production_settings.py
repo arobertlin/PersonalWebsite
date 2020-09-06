d1631887a26d695527a2eea2079fecdcce7d0024
Andrew Lin <andrewlin1722@gmail.com>
2020-09-05 21:56:22 -0500

    trying to figure out module not found error

--- my_personal_website/production_settings.py
+++ my_personal_website/production_settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 #update this to be heroku
 ALLOWED_HOSTS = ['0.0.0.0', '.herokuapp.com']
 
-BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
